e72be11d04cd92e11c8176ddf712af14be7500f8
superigrooper <superigrooper@gmail.com>
2023-01-20 17:22:50 +0300

    Секция 'Сертификаты' - готов адаптив, застилизованы кнопки слайдера

--- src/js/modules/certs.js
+++ src/js/modules/certs.js
@@ -1,14 +1,26 @@
+
+//  Swiper certs
 const gap = 32;
 const certsSwiper = new Swiper('.certs__swiper', {
-  // loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: gap,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: gap
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: gap
+    },
+  },
   navigation: {
     nextEl: '.certs__swiper-nav--next',
     prevEl: '.certs__swiper-nav--prev'
   }
 });
 
+// Full view
 Fancybox.bind("#grouped-images img", {
   groupAll: true,
 });
